d1d578532bba95874b598f60395ba2bffae49b0c
kksusa <kksusa1@gmail.com>
2022-08-30 16:53:42 +0500

    Задание 1

--- TSK_1/Program.cs
+++ TSK_1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CheckNumbers(string param)
+{
+    while (true)
+    {
+        Console.Write($"{param}: ");
+        if (int.TryParse(Console.ReadLine()!, out int number) && number > 0)
+        {
+            return number;
+        }
+        else
+        {
+            Console.WriteLine("Число введено неправильно.");
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] CreateArrayWithRandomNumbers(int m, int n)
+{
+    double[,] result = new double[m,n];
+    var random = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = Math.Round(random.NextDouble() * 200 - 100, 2);
+        }
+    }
+    return result;
+}
+
+PrintArray(CreateArrayWithRandomNumbers(CheckNumbers("Введите число строк"), CheckNumbers("Введите число столбцов")));
